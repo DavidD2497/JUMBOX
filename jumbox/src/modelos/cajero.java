23a210899ef7b91f29ba7f2b7aaa5be40760fd73
DavidD2497 <david.dasilva@davinci.edu.ar>
2024-05-22 16:32:40 -0300

    funcion registrarVentas

--- jumbox/src/modelos/cajero.java
+++ jumbox/src/modelos/cajero.java
@@ -5,12 +5,19 @@ import javax.swing.JOptionPane;
 
 public class cajero extends empleado {
     private int idCajero;
+<<<<<<< Updated upstream:jumbox/src/modelos/cajero.java
     private inventarioSucursal inventarioSucursal;
 
     public cajero(String nombre, String contraseña, int idCajero, inventarioSucursal inventarioSucursal) {
+=======
+    private InventarioSucursal inventarioSucursal;
+    private LinkedList<Venta> listaVentas = new LinkedList<>();
+    
+
+    public Cajero(String nombre, String contraseña, int idCajero) {
+>>>>>>> Stashed changes:jumbox/src/modelos/Cajero.java
         super(nombre, contraseña);
         this.idCajero = idCajero;
-        this.inventarioSucursal = inventarioSucursal;
     }
 
     public int getIdCajero() {
@@ -35,6 +42,34 @@ public class cajero extends empleado {
             }
         }
         JOptionPane.showMessageDialog(null, "Producto no encontrado en el inventario.");
+    }
+    
+    public void registrarVenta(LinkedList<DetalleVenta> detalles, String tipoPago) {
+        double montoTotal = 0;
+
+        for (DetalleVenta detalleVenta : detalles) {
+            int idProducto = detalleVenta.getIdProducto();
+            int cantidadVenta = detalleVenta.getCantidad();
+            LinkedList<DetalleInventario> listaInventario = inventarioSucursal.getListaInventario();
+
+            for (DetalleInventario detalleInventario : listaInventario) {
+                if (detalleInventario.getProducto().getIdProducto() == idProducto) {
+                    if (detalleInventario.getCantidad() >= cantidadVenta) {
+                        detalleInventario.setCantidad(detalleInventario.getCantidad() - cantidadVenta);
+                        montoTotal += detalleVenta.getMonto();
+                    } else {
+                        JOptionPane.showMessageDialog(null, "No hay suficiente inventario para el producto: " + detalleInventario.getProducto().getNombreProducto());
+                        return;
+                    }
+                }
+            }
+        }
+
+        int idVenta = listaVentas.size() + 1;
+        Venta nuevaVenta = new Venta(idVenta, montoTotal, tipoPago, detalles);
+        listaVentas.add(nuevaVenta);
+
+        JOptionPane.showMessageDialog(null, "Venta registrada con éxito. Monto total: " + montoTotal);
     }
 }
 
