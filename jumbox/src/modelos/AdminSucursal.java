10753738c7e435d4f41b9d9a7ec19f3fb9a5631d
tomasrudilla05 <tomas.rudilla@davinci.edu.ar>
2024-05-28 00:20:27 -0300

    .

--- jumbox/src/modelos/AdminSucursal.java
+++ jumbox/src/modelos/AdminSucursal.java
@@ -33,17 +33,23 @@ public class AdminSucursal extends Empleado {
 	}
 
 	public void RegistroEntrada(int idProducto, int cantidadEntrada) {
-		
-		 InventarioSucursalControlador inventarioSucursalControlador = new InventarioSucursalControlador();
-		 int cantidadDisponible = inventarioSucursalControlador.getCantidadDisponible(idProducto);
-		 if (cantidadEntrada==0) {
-			JOptionPane.showMessageDialog(null, "No se va a registrar ninguna entrada si la cantidad es 0");
-		}
-		 int cantidadTotal = cantidadDisponible + cantidadEntrada;
-		 inventarioSucursalControlador.actualizarCantidadProducto(idProducto, cantidadTotal);
-		 JOptionPane.showMessageDialog(null, "Entrada de " + cantidadEntrada + " unidades del producto con ID: " + idProducto + " registrada con éxito.");
-		       }
-		    
+	    
+	    InventarioSucursalControlador inventarioSucursalControlador = new InventarioSucursalControlador();
+	    int cantidadDisponible = inventarioSucursalControlador.getCantidadDisponible(idProducto);
+	    if (cantidadEntrada == 0) {
+	        
+	        JOptionPane.showMessageDialog(null, "No se va a registrar ninguna entrada si la cantidad es 0");
+	    }
+	    
+	    int cantidadTotal = cantidadDisponible + cantidadEntrada;
+	    inventarioSucursalControlador.actualizarCantidadProducto(idProducto, cantidadTotal);
+	    
+	    JOptionPane.showMessageDialog(null, "Entrada de " + cantidadEntrada + " unidades del producto con ID: " + idProducto 
+	    + " registrada con éxito.");
+	
+	}
+	
+	
 		
 	public void SolicitarPedido() {
 		InventarioSucursalControlador inventarioSucursalControlador = new InventarioSucursalControlador();
@@ -51,7 +57,7 @@ public class AdminSucursal extends Empleado {
 	}
 
 	public void MostrarSolicitudPedido() {
-
+		
 	}
 
 
