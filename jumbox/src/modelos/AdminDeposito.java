c25cc12991e7af279fa6cdefa26826d4ff5f954d
VictoriaLedesma <victoria.ledesma@davinci.edu.ar>
2024-05-29 18:47:09 -0300

    gs

--- jumbox/src/modelos/AdminDeposito.java
+++ jumbox/src/modelos/AdminDeposito.java
@@ -55,9 +55,9 @@ public class AdminDeposito extends Empleado {
 				+ "/n Contraseña: " + this.getContraseña() + "/n Id del Deposito: " + this.idAdminDepo);
 	}
 
-	public void confirmarSolicitudDePedido(LinkedList<DetallePedido> listaDetalle) {
+	public void confirmarSolicitudDePedido(LinkedList<DetallePedido> listaDetalle, LocalDate fechaEntrega) {
 		if (listaDetalle.isEmpty()) {
-			JOptionPane.showMessageDialog(null, "No hay detalles de pedido para confirmar.");
+			JOptionPane.showMessageDialog(null, "Error. Se creó una lista vacía.");
 			return;
 		}
 
@@ -70,22 +70,34 @@ public class AdminDeposito extends Empleado {
 
 			if (!detalleDepositoControlador.existeProducto(idProducto, cantidadSolicitada)) {
 				pedidoConfirmado = false;
-				JOptionPane.showMessageDialog(null, "El producto: " + idProducto + " no esta disponible");
+				JOptionPane.showMessageDialog(null,
+						"Error. El producto con ID " + idProducto + " no está disponible en cantidad suficiente.");
 				break;
 			}
 		}
 
 		if (pedidoConfirmado) {
+			if (fechaEntrega == null) {
+				JOptionPane.showMessageDialog(null, "Error. No se ingresó la fecha de entrega.");
+				return;
+			}
+
+			LocalDate fechaActual = LocalDate.now();
+			if (!fechaEntrega.isAfter(fechaActual.plusDays(2))) {
+				JOptionPane.showMessageDialog(null,
+						"Error. La fecha de entrega debe ser al menos 2 días posterior a la fecha actual.");
+				return;
+			}
+
 			PedidoControlador pedidoControlador = new PedidoControlador();
-			Pedido nuevoPedido = new Pedido(LocalDate.now());
+			Pedido nuevoPedido = new Pedido(fechaEntrega);
 			pedidoControlador.addPedido(nuevoPedido);
 			int codigoPedido = nuevoPedido.getCodigoPedido();
 			pedidoControlador.actualizarEstadoPedido(codigoPedido, "Confirmado");
-			JOptionPane.showMessageDialog(null,
-					"El pedido fue confirmado. Todos los productos están disponibles");
+			JOptionPane.showMessageDialog(null, "Solicitud de Pedido con éxito.");
 		} else {
 			JOptionPane.showMessageDialog(null,
-					"No fue confirmado el pedido. Al menos uno de los productos solicitados no está disponible");
+					"Error. No fue confirmado el pedido. Al menos uno de los productos solicitados no está disponible.");
 		}
 	}
 
