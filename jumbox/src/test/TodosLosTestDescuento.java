0e328f5b1002b4b49d48b64c44d4c399dc7f3e95
VictoriaLedesma <victoria.ledesma@davinci.edu.ar>
2024-06-11 17:09:21 -0300

    chgs

---
+++ jumbox/src/test/TodosLosTestDescuento.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	GuardarDescuentoTest.class,
+	EditarDescuentoTest.class,
+	MostrarDescuentoTest.class,
+	EliminarDescuentoTest.class
+})
+
+public class TodosLosTestDescuento {
+
+}
