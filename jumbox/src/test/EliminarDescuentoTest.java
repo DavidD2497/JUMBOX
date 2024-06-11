0df53bba74cd5a421bc862c4c2497e0cef981589
VictoriaLedesma <victoria.ledesma@davinci.edu.ar>
2024-06-11 17:26:26 -0300

    chg

---
+++ jumbox/src/test/EliminarDescuentoTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import modelos.AdminSucursal;
+import modelos.Descuento;
+
+public class EliminarDescuentoTest {
+	private AdminSucursal adminSucursal;
+
+	@Test
+	public void testEliminarDescuento() {
+		Descuento descuento = new Descuento(1, 10);
+		adminSucursal.getDescuentos().add(descuento);
+
+		adminSucursal.eliminarDescuento(0);
+
+		assertTrue(adminSucursal.getDescuentos().isEmpty());
+	}
+}
